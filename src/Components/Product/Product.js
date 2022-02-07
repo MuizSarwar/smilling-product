39ba9ba50e35732eb5d5fe55e103d6e188175d50
MuizSarwar <you@example.com>
2022-02-07 12:32:52 +0600

    some additional changes

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -13,18 +13,26 @@ const Product = (props) => {
             <div className="product-details">
                 <h3>{name}</h3>
                 <p>by:{seller}</p>
+
                 <div className='product-others-details'>
+
                     <div>
                         <p>price:{price}</p>
                         <p><small>Only {stock} left in stock,order soon </small></p>
                         <button className='style-btn'>Add to cart</button>
 
                     </div>
+
                     <div>
                         <p>star:{star}</p>
+
                         <div>
-                            {features.map(feature => <p>{feature.description}:{feature.value}</p>)}
+                            <h4>Features:</h4>
+                            <ul className='product-features'>
+                                {features.map(feature => <li>{feature.description}:{feature.value}</li>)}
+                            </ul>
                         </div>
+                        
                     </div>
                 </div>
                 
